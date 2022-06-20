ed45bfecc4b2f7e143b2e57cf61722a5bc2d2b2b
fabiano <fabiano.rocha@penseavanti.com.br>
2022-06-19 23:18:16 -0300

    first commit

---
+++ src/components/Untitled-1.js
@@ -0,0 +1,13 @@
+{posts.length === 0 ?
+        <p>There are no blog posts.</p>
+      :
+        posts.map(post => {
+          return (
+            <p key={post.id}>
+              <b>{post.author.username}</b> &mdash; {post.timestamp}
+              <br />
+              {post.text}
+            </p>
+          );
+        })
+      }
